6417bc10729e891edf72fd1a551dc6cbd96fd844
shuohangwang <shuohangw@gmail.com>
2016-11-02 19:34:33 +0800

    update

--- util/loadFiles.lua
+++ util/loadFiles.lua
@@ -1,3 +1,10 @@
+--[[
+Copyright 2015 Singapore Management University (SMU). All Rights Reserved.
+
+Permission to use, copy, modify and distribute this software and its documentation for purposes of research, teaching and general academic pursuits, without fee and without a signed licensing agreement, is hereby granted, provided that the above copyright statement, this paragraph and the following paragraph on disclaimer appear in all copies, modifications, and distributions.  Contact Singapore Management University, Intellectual Property Management Office at iie@smu.edu.sg, for commercial licensing opportunities.
+
+This software is provided by the copyright holder and creator “as is” and any express or implied warranties, including, but not Limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.  In no event shall SMU or the creator be liable for any direct, indirect, incidental, special, exemplary or consequential damages, however caused arising in any way out of the use of this software.
+]]
 local stringx = require 'pl.stringx'
 require 'debug'
 require 'paths'
