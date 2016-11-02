63af2ef581463d367a07a5fc0a66262707f6fa75
shuohangwang <shuohangw@gmail.com>
2016-11-02 20:30:50 +0800

    update

--- trainedmodel/evaluation/squad/txt2js.py
+++ trainedmodel/evaluation/squad/txt2js.py
@@ -3,10 +3,10 @@ Copyright 2015 Singapore Management University (SMU). All Rights Reserved.
 
 Permission to use, copy, modify and distribute this software and its documentation for purposes of research, teaching and general academic pursuits, without fee and without a signed licensing agreement, is hereby granted, provided that the above copyright statement, this paragraph and the following paragraph on disclaimer appear in all copies, modifications, and distributions.  Contact Singapore Management University, Intellectual Property Management Office at iie@smu.edu.sg, for commercial licensing opportunities.
 
-This software is provided by the copyright holder and creator “as is” and any express or implied warranties, including, but not Limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.  In no event shall SMU or the creator be liable for any direct, indirect, incidental, special, exemplary or consequential damages, however caused arising in any way out of the use of this software.
+This software is provided by the copyright holder and creator "as is" and any express or implied warranties, including, but not Limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.  In no event shall SMU or the creator be liable for any direct, indirect, incidental, special, exemplary or consequential damages, however caused arising in any way out of the use of this software.
 '''
-import sys 
-reload(sys) 
+import sys
+reload(sys)
 sys.setdefaultencoding('utf-8')
 import json
 import string
@@ -24,7 +24,7 @@ for c in js["data"]:
 		context_word = word_tokenize(context)
 		for qa in p["qas"]:
 			question_id = qa["id"]
-			
+
 			pred = fpr.readline().strip()
 			newpred = []
 			pre_word = ''
@@ -41,7 +41,7 @@ for c in js["data"]:
 					newpred.append(word)
 				pre_word = word
 			predictions[question_id] = ' '.join(newpred)
-			
+
 predictions_js = json.dumps(predictions)
 fpw = open(sys.argv[3], 'w')
 fpw.write(predictions_js)
