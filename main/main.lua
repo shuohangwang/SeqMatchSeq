35d1ac6932f280734da56f64a358561c0fea680f
shuohangwang <shuohangw@gmail.com>
2016-11-02 19:33:12 +0800

    update

--- main/main.lua
+++ main/main.lua
@@ -1,8 +1,15 @@
+--[[
+Copyright 2015 Singapore Management University (SMU). All Rights Reserved.
+
+Permission to use, copy, modify and distribute this software and its documentation for purposes of research, teaching and general academic pursuits, without fee and without a signed licensing agreement, is hereby granted, provided that the above copyright statement, this paragraph and the following paragraph on disclaimer appear in all copies, modifications, and distributions.  Contact Singapore Management University, Intellectual Property Management Office at iie@smu.edu.sg, for commercial licensing opportunities.
+
+This software is provided by the copyright holder and creator “as is” and any express or implied warranties, including, but not Limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.  In no event shall SMU or the creator be liable for any direct, indirect, incidental, special, exemplary or consequential damages, however caused arising in any way out of the use of this software.
+]]
 require 'nngraph'
 require 'optim'
 require 'debug'
 torch.setdefaulttensortype('torch.FloatTensor')
-transition = {}
+seqmatchseq = {}
 tr = require '../util/loadFiles'
 include '../util/utils.lua'
 
@@ -71,7 +78,7 @@ if opt.task == 'snli' or opt.task == 'wikiqa' then test_dataset = tr:loadData('t
 local dev_dataset = tr:loadData('dev', opt.task)
 torch.manualSeed(opt.seed)
 
-local model_class = transition[opt.model]
+local model_class = seqmatchseq[opt.model]
 
 local model = model_class(opt)
 
--- main/mainDt.lua
+++ main/mainDt.lua
@@ -1,4 +1,10 @@
+--[[
+Copyright 2015 Singapore Management University (SMU). All Rights Reserved.
 
+Permission to use, copy, modify and distribute this software and its documentation for purposes of research, teaching and general academic pursuits, without fee and without a signed licensing agreement, is hereby granted, provided that the above copyright statement, this paragraph and the following paragraph on disclaimer appear in all copies, modifications, and distributions.  Contact Singapore Management University, Intellectual Property Management Office at iie@smu.edu.sg, for commercial licensing opportunities.
+
+This software is provided by the copyright holder and creator “as is” and any express or implied warranties, including, but not Limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.  In no event shall SMU or the creator be liable for any direct, indirect, incidental, special, exemplary or consequential damages, however caused arising in any way out of the use of this software.
+]]
 include './init.lua'
 
 
@@ -11,7 +17,7 @@ function worker()
     local ivocab = tr:loadiVocab(opt.task)
     opt.numWords = #ivocab
 
-    local model_class = transition[opt.model]
+    local model_class = seqmatchseq[opt.model]
     local model = model_class(opt)
 
     parallel.print('Im a worker, my ID is: ' .. parallel.id .. ' and my IP: ' .. parallel.ip)
@@ -75,7 +81,7 @@ end
 function parent()
     parallel.print('Im the parent, my ID is: ' .. parallel.id)
     tr:init(opt)
-    local model_class = transition[opt.model]
+    local model_class = seqmatchseq[opt.model]
     local model = model_class(opt)
 
     parallel.nfork(opt.num_processes)
